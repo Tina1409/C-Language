95c258e0ed57a9a6fdaeed582b1f9f85e1c690b6
Tina <tinav1409@gmail.com>
2022-09-18 02:46:24 +0530

    Syntax: Printf and Scanf

---
+++ Printf.c
@@ -0,0 +1,7 @@
+//printf//
+int main()
+{
+    int a=6,b=5;
+	int sum=a+b;
+	printf("sum of %d and %d is: %d ",a,b,sum);
+}
---
+++ scanf.c
@@ -0,0 +1,8 @@
+//scanf//
+int main(){
+	
+   int x;
+   printf("Enter a number: ");
+   scanf("%d",&x); 
+   printf("Value of x is: %d ",x);
+}
