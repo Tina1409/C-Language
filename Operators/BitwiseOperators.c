124dde68cbf62e2227ba894f718031af82bf2319
Tina <tinav1409@gmail.com>
2022-09-22 21:22:42 +0530

    bitwiseOperators

---
+++ Operators/BitwiseOperators.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main(){
+//ADD
+/*
+	int x;
+	x=23&56;
+ 	printf("%d",x);
+*/
+
+//OR
+/*
+	int x;
+	x=23|56;
+ 	printf("%d",x);
+*/
+
+//XOR
+/*
+	int x;
+	x=23^56;
+ 	printf("%d",x);
+*/
+
+//LEFT SHIFT
+/*
+    int x;
+ 	x=56<<3;
+ 	printf("%d",x);
+*/
+ 	
+//RIGHT SHIFT
+
+	int x;
+ 	x=56>>3;
+ 	printf("%d",x);
+
+}
