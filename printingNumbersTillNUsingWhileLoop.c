ee9e9a2b81f26b1655b5b0b5c9699748cbed8bea
Tina <tinav1409@gmail.com>
2023-01-09 00:11:57 +0530

    printingNumbersTillNUsingWhileLoop

---
+++ printingNumbersTillNUsingWhileLoop.c
@@ -0,0 +1,11 @@
+//printing numbers till n using while loop
+#include"stdio.h"
+int main(){
+	int i,n;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	while(i<=n){
+		printf("%d\n",i);
+		i++;
+	}
+}
