8b0687e400eb795cfec33d950491922046d208e3
Tina <tinav1409@gmail.com>
2023-01-13 00:04:25 +0530

    nestingOfForLoops

---
+++ nestingOfForLoops.c
@@ -0,0 +1,14 @@
+//patterns
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	for(j=0;j<=5;j++){
+		for(i=0;i<=5;i++){
+			for(k=0;k<=4;k++){
+				printf("*");
+	        }
+	        printf("\t");
+		}
+	printf("\n");
+	}
+}
