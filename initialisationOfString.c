5613a72007211f44f186ebfcc5e70df5abb38359
Tina <tinav1409@gmail.com>
2023-01-20 17:58:57 +0530

    initialisationOfString

---
+++ initialisationOfString.c
@@ -0,0 +1,34 @@
+//INITIALIZATION OF A STRING
+#include"stdio.h"
+int main(){
+	/*
+	char s1[50]="H.No-126,Gurgaon";
+	printf("%s",s1);
+	*/
+	
+	/*
+	int a;
+	char s2[]="12345678910";
+	a=sizeof(s2);
+	printf("%s",s2);
+	printf("\n%d",a);
+	*/
+	
+	/*
+	char s3[5]="Hello";
+	printf("%s",s3);
+	*/
+	
+	int a;
+	char s4[]={'H','e','l','l','o','\0'};
+	printf("%s",s4);
+	a=sizeof(s4);
+	printf("\n%d",a);
+	
+	
+	/*
+	char s5[15]="Tina Verma";
+	printf("%s",s5);
+	*/
+	
+}
