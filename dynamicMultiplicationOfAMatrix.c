69e95074873b4dcc92e69c0840e2bd7d993250e5
Tina <tinav1409@gmail.com>
2023-01-21 23:53:39 +0530

    dynamicMultiplicationOfAMatrix

---
+++ dynamicMultiplicationOfAMatrix.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#DEFINE N 50
+int main(){
+	int A[N][N],B[N][N],C[N][N],i,j,k,p,q,r,s;
+	int sum;
+	printf("Enter elements of array 1: ");
+	scanf("%d",&A[p][q]);
+	printf("Enter elements of array 2: ");
+	scanf("%d",&B[r][s]);
+	
+	
+}
