52b84c68c962f189a616b74279c6a6579ec286a5
Tina <tinav1409@gmail.com>
2023-01-22 20:11:33 +0530

    putsInString

---
+++ putsWithString.c
@@ -0,0 +1,11 @@
+//PUTS IN STRING
+#include<Stdio.h>
+int main(){
+	char name[30];
+	printf("Enter name: ");
+	scanf("%s",name);
+	printf("%s", name);
+	printf("%s", name);
+	puts(name);
+	puts(name); //PUTS INSERTS A NEW LINE IMMEDIATELY
+}
