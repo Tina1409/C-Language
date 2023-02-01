a6f5d92768b108c8ac5d348fe5492974bcde60ef
Tina <tinav1409@gmail.com>
2023-02-01 23:49:22 +0530

    multiply

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#inculde"stdio.h"
+int main(){
+	int a,b,mult;
+	printf("Enter first number: ");
+	scanf("%d",&a);
+	printf("Enter second number: ");
+	scanf("%d",&b);
+	mult=a*b;
+	printf("Multiplication of %d and %d is %d",a,b,mult);
+}
